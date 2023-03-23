ff498f5aa3ad5b1d24d479a63c057aec541c9ee0
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-03-23 19:33:46 +0100

    add example.html

--- src/js/pixabay.js
+++ src/js/pixabay.js
@@ -26,5 +26,46 @@
 // const axios = require('axios');
 import axios from 'axios';
 
-const API_KEY = '34465474 - c3837bc3938f4efd53294c219';
+const API_KEY = '34465474-c3837bc3938f4efd53294c219';
 const BASE_URL = 'https://pixabay.com/api/';
+
+export default class PixabayApiServise {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.per_page = 40;
+  }
+
+  async fetchPicture() {
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: this.per_page,
+    });
+
+    const url = `${BASE_URL}?${searchParams}`;
+    this.incrementPage();
+    return await axios.get(url);
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get per_Page() {
+    return this.per_page;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
